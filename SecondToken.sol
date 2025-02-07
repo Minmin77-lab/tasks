82e49a73a4ce44dcaa0e27c414b725f24bd45f5f
Minmin77-lab <panahovaarina52@gmail.com>
2025-02-07 13:21:37 +0500

    Create SecondToken.sol

---
+++ SecondToken.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./IERC20.sol"; 
+
+contract MyToken is IERC20 {
+    string public name = "YourToken";
+    string public symbol = "YTK";
+    
+    function decimals() external pure returns (uint) {
+        return 18; 
+    }
+
+    address private owner;
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(uint256 initialSupply) {
+        _mint(msg.sender, initialSupply); 
+        owner = msg.sender;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view  returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public  returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint256) {
+        return _allowances[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        require(_allowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        _allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _mint(address account, uint256 amount) public  {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _totalSupply += amount * (10 ** uint256(18)); 
+        _balances[account] += amount * (10 ** uint256(18)); 
+        emit Transfer(address(0), account, amount * (10 ** uint256(18))); 
+    }
+
+    function burn(uint256 amount) public {
+        require(_balances[msg.sender] >= amount, "ERC20: burn amount exceeds balance");
+        _burn(msg.sender, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+        _totalSupply -= amount * (10 ** uint256(18)); 
+        _balances[account] -= amount * (10 ** uint256(18)); 
+        emit Transfer(account, address(0), amount * (10 ** uint256(18))); 
+    }
+
+    function transferFromOwner(address sender, address recipient, uint256 amount) public {
+        require(msg.sender == owner, "Only owner can call this function");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+}
